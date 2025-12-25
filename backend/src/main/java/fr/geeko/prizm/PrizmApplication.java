0c342835a99003d1c33f6861cac8062f2fdeab56
vtruyen <victor.truyen.etu@univ-lille.fr>
2025-12-25 11:46:33 +0100

    Initialize project

---
+++ backend/src/main/java/fr/geeko/prizm/PrizmApplication.java
@@ -0,0 +1,13 @@
+package fr.geeko.prizm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrizmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrizmApplication.class, args);
+	}
+
+}
